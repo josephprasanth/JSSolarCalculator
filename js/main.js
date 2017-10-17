a559c14ce0bf43e2d660cb5b89e3a99d8936dc64
josephprasanth <prasanthjoseph_77@yahoo.com>
2017-10-17 19:29:09 +0200

    Adding in The functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-alert('hello world from joseph');
+// Javascript Document
+
+function floor(diameter) {
+  var radius = diameter/2;
+  var floorArea = Math.PI * (radius * radius);
+  return(floorArea);
+}//end of Function
+
+function walls(diameter, height) {
+  var radius = diameter/2;
+  var circumference = 2 * Math.PI * radius;
+  var wallArea = circumference * height;
+  return(wallArea);
+}//end of Function
+
+function materialsNeeded() {
+
+  var d = document.getElementById("across").value;
+  var h = document.getElementById("height").value;
+  var carpetNeeded = Math.ceil(floor(d));
+  var paintNeeded = Math.ceil(walls(d, h));
+  console.log(carpetNeeded);
+  console.log(paintNeeded);
+}//end of Function
